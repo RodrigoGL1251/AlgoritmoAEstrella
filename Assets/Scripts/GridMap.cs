79c4ce06332b9d250cc731aa6ceaff24bd2142fa
04WS-LABZ2-09\Alumno <emmanuel.maciel0208@gmail.com>
2023-10-17 13:08:24 -0600

    Cositas de mas

--- Assets/Scripts/GridMap.cs
+++ Assets/Scripts/GridMap.cs
@@ -9,6 +9,7 @@ public class GridMap : MonoBehaviour
     public Vector2 gridWorldSize;
     public float nodeRadius;
     Node[,] grid;
+    public List<Node> path;
     float nodeDiameter;
     int gridSizeX, gridSizeY;
 
@@ -32,9 +33,33 @@ public class GridMap : MonoBehaviour
             {
                 Vector3  worldPoint = worldBottomLeft + Vector3.right *(x * nodeDiameter + nodeRadius) + Vector3.forward *(y*nodeDiameter+nodeRadius);
                 bool walkable = !(Physics.CheckSphere(worldPoint,nodeRadius,unawalkableMask));
-                grid[x,y] = new Node(walkable,worldPoint);
+                grid[x,y] = new Node(walkable,worldPoint, x, y);
             }
         }
+    }
+
+    public List<Node> GetNeighbours(Node node)
+    {
+        List<Node> neighbours = new List<Node>();
+
+        for (int x = -1; x <= 1; x++)
+        {
+            for (int y = -1; y <= 1; y++)
+            {
+                if (x == 0 && y == 0)
+                    continue;
+
+                int checkX = node.gridX + x;
+                int checkY = node.gridY + y;
+
+                if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY)
+                {
+                    neighbours.Add(grid[checkX, checkY]);
+                }
+            }
+        }
+
+        return neighbours;
     }
 
     public Node NodeFromWorldPoint(Vector3 worldPosition)
